bf9530cdd33a97757600f2b18625169edba5642a
jhs512 <jangka512@gmail.com>
2026-05-21 12:19:02 +0900

    031

--- src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -88,4 +88,31 @@ public class ApiV1PostController {
                 )
         );
     }
+
+
+    public record PostModifyReqBody(
+            @NotBlank
+            @Size(min = 2, max = 100)
+            String title,
+            @NotBlank
+            @Size(min = 2, max = 5000)
+            String content
+    ) {
+    }
+
+    @PutMapping("/{id}")
+    @Transactional
+    public RsData<Void> modify(
+            @PathVariable int id,
+            @RequestBody @Valid PostModifyReqBody reqBody
+    ) {
+        Post post = postService.findById(id).get();
+
+        postService.modify(post, reqBody.title, reqBody.content);
+
+        return new RsData<>(
+                "200-1",
+                "%d번 글이 수정되었습니다.".formatted(post.getId())
+        );
+    }
 }
