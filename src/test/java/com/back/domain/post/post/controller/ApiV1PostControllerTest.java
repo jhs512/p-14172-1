168849657ca87281285c0c0f3ad621eb762978c2
jhs512 <jangka512@gmail.com>
2026-05-21 14:17:26 +0900

    033-2

--- src/test/java/com/back/domain/post/post/controller/ApiV1PostControllerTest.java
+++ src/test/java/com/back/domain/post/post/controller/ApiV1PostControllerTest.java
@@ -1,4 +1,40 @@
 package com.back.domain.post.post.controller;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@ActiveProfiles("test") // 테스트 환경에서는 test 프로파일을 활성화합니다.
+@SpringBootTest // 스프링부트 테스트 클래스임을 나타냅니다.
+@AutoConfigureMockMvc // MockMvc를 자동으로 설정합니다, POSTMAN 처럼 요청을 보낼 수 있음
+@Transactional // 각 테스트 메서드가 종료되면 롤백됩니다.
 public class ApiV1PostControllerTest {
+    @Autowired
+    private MockMvc mvc; // MockMvc를 주입받습니다, @AutoConfigureMockMvc 덕분에 가능
+
+    // 글 작성 테스트
+    @Test
+    @DisplayName("글 쓰기")
+    void t1() throws Exception {
+        // 글 작성 요청을 보냅니다.
+        mvc
+                .perform(
+                        post("/api/v1/posts")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("""
+                                        {
+                                            "title": "제목",
+                                            "content": "내용"
+                                        }
+                                        """)
+                );
+    }
 }
