7f8d03d0227b221bde75cb64912b151bcefc7021
jhs512 <jangka512@gmail.com>
2026-05-21 15:07:01 +0900

    mokito

--- src/main/java/com/back/global/initData/BaseInitData.java
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -8,8 +8,10 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Profile;
 import org.springframework.transaction.annotation.Transactional;
 
+@Profile("!test")
 @Configuration
 @RequiredArgsConstructor
 public class BaseInitData {
