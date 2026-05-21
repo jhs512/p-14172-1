d56c7b56d3a6a4c23164556fb612d244d293d8e8
jhs512 <jangka512@gmail.com>
2026-05-21 14:20:11 +0900

    033-3

--- src/test/java/com/back/domain/post/post/controller/ApiV1PostControllerTest.java
+++ src/test/java/com/back/domain/post/post/controller/ApiV1PostControllerTest.java
@@ -11,20 +11,19 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
-@ActiveProfiles("test") // 테스트 환경에서는 test 프로파일을 활성화합니다.
-@SpringBootTest // 스프링부트 테스트 클래스임을 나타냅니다.
-@AutoConfigureMockMvc // MockMvc를 자동으로 설정합니다, POSTMAN 처럼 요청을 보낼 수 있음
-@Transactional // 각 테스트 메서드가 종료되면 롤백됩니다.
+@ActiveProfiles("test")
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
 public class ApiV1PostControllerTest {
     @Autowired
-    private MockMvc mvc; // MockMvc를 주입받습니다, @AutoConfigureMockMvc 덕분에 가능
+    private MockMvc mvc;
 
-    // 글 작성 테스트
     @Test
     @DisplayName("글 쓰기")
     void t1() throws Exception {
-        // 글 작성 요청을 보냅니다.
         mvc
                 .perform(
                         post("/api/v1/posts")
@@ -35,6 +34,7 @@ public class ApiV1PostControllerTest {
                                             "content": "내용"
                                         }
                                         """)
-                );
+                )
+                .andDo(print()); // 응답결과를 출력합니다.
     }
 }
