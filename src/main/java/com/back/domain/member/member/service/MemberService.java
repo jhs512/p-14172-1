d52349c259e66ac1cfea693ef5e966ce333f8677
jhs512 <jangka512@gmail.com>
2026-05-27 12:57:02 +0900

    070-4

--- src/main/java/com/back/domain/member/member/service/MemberService.java
+++ src/main/java/com/back/domain/member/member/service/MemberService.java
@@ -5,21 +5,18 @@ import com.back.domain.member.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
 
     public Member join(String username, String password, String name) {
-        Optional<Member> opMember = memberRepository.findByUsername(username);
-
-        if (opMember.isPresent()) {
-            throw new IllegalArgumentException(
-                    "%s(은)는 이미 사용중인 username 입니다.".formatted(username)
-            );
-        }
+        memberRepository.findByUsername(username)
+                .ifPresent(_ -> {
+                    throw new IllegalArgumentException(
+                            "%s(은)는 이미 사용중인 username 입니다.".formatted(username)
+                    );
+                });
 
         Member member = new Member(username, password, name);
 
