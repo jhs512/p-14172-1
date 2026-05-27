2c31dcfc479c28ea34b30bcda46f1ee151aaf785
jhs512 <jangka512@gmail.com>
2026-05-27 12:37:43 +0900

    070-2

--- src/main/java/com/back/domain/member/member/service/MemberService.java
+++ src/main/java/com/back/domain/member/member/service/MemberService.java
@@ -1,5 +1,6 @@
 package com.back.domain.member.member.service;
 
+import com.back.domain.member.member.entity.Member;
 import com.back.domain.member.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,4 +9,10 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
+
+    public Member join(String username, String password, String name) {
+        Member member = new Member(username, password, name);
+
+        return memberRepository.save(member);
+    }
 }
